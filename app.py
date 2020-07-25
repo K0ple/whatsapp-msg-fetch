27e290133163fa9639185946afc6ee1e6004e2e3
Satyaki Suman <satyaki.suman@gmail.com>
2020-07-25 22:18:13 +0530

    added parsing for location

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 import logging
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
+from flask_pymongo import pymongo
 # from flask_googletrans import translator
 
 app = Flask(__name__)
-ts = translator(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+# mongo = PyMongo(app)
+# ts = translator(app)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -18,11 +21,16 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_num = request.values.get('From','')
-    incoming_msg = request.values.get('Body', '').lower()
+    incoming_num = int(incoming_num[12:])
+    incoming_msg = request.values.get('Body', '').lower() + ' '
+
 
     #logged the values to console to see all the values
     print(request.values)
-    
+
+    # client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.vz4wz.mongodb.net/cfg?retryWrites=true&w=majority')
+    # print(myclient.list_database_names())
+
     app.logger.info(incoming_msg)
     app.logger.info(incoming_num)
     resp = MessagingResponse()
@@ -31,17 +39,20 @@ def bot():
     # incoming_msg_translated = translator(incoming_msg)
     # print(incoming_msg_translated)
 
-    if 'location' not in incoming_msg:
+    if 'location :' not in incoming_msg:
         #return request if location: keyword is not included in the string
         # placeholder to implement translation
-        msg.body('Include location in your message. Example: Saw a garbage dump on location SP road.')
+        msg.body('Include location : in your message. Example: Saw a garbage dump on location : SP road')
 
-    if 'location' in incoming_msg:
+    if 'location : ' in incoming_msg:
         # return request if location: keyword is included in string
         # placeholder toimplement translation
         msg.body('Thanks for registering your complaint with us!')
+        incoming_loc = incoming_msg.split(':')
+        incoming_loc1 = incoming_loc[1]
         # placeholder for sending get request to the main django service
-        task = {"number": incoming_num, "description": incoming_msg }
+        task = {"number": incoming_num, "description": incoming_msg, "location": incoming_loc1 }
+        # log the json to the console
         print(task)
         # resp = requests.post('https://todolist.example.com/tasks/', json=task)
         # if resp.status_code != 201:
