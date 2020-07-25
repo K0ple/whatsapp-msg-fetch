73cd09e245f151ea17536b853d2f124b87bbd2dc
Satyaki Suman <satyaki.suman@gmail.com>
2020-07-25 19:06:35 +0530

    removed translation

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, request
 import logging
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
-from googletrans import Translator
+# from flask_googletrans import translator
 
 app = Flask(__name__)
+ts = translator(app)
 
 logging.basicConfig(level=logging.DEBUG)
 
-def translator(message):
-    translator = Translator()
-    translator.translate(message)
+# def translator(message):
+#     translator = Translator()
+#     return translator.translate(message)
+    
 
 #POST route to fetch messages from WhatsApp
 @app.route('/bot', methods=['POST'])
@@ -18,7 +20,7 @@ def bot():
     incoming_num = request.values.get('From','')
     incoming_msg = request.values.get('Body', '').lower()
 
-    #logged the values to console to seall the values
+    #logged the values to console to see all the values
     print(request.values)
     
     app.logger.info(incoming_msg)
@@ -26,14 +28,15 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
 
-    incoming_msg_translated = translator(incoming_msg) 
+    # incoming_msg_translated = translator(incoming_msg)
+    # print(incoming_msg_translated)
 
-    if 'location' or 'place' not in incoming_msg_translated:
+    if 'location' not in incoming_msg:
         #return request if location: keyword is not included in the string
         # placeholder to implement translation
         msg.body('Include location in your message. Example: Saw a garbage dump on location SP road.')
 
-    if 'location' or 'place' in incoming_msg_translated:
+    if 'location' in incoming_msg:
         # return request if location: keyword is included in string
         # placeholder toimplement translation
         msg.body('Thanks for registering your complaint with us!')
